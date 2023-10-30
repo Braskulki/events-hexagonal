94e0ff44feeb74e1cdb8e11c379ee5998e8ac1b5
Yuri Simoes Braskulki <yuri.simoes@mblabs.com.br>
2023-10-30 11:56:15 -0300

    chore: address repository interface

--- src/domain/repositories/address-repository.interface.ts
+++ src/domain/repositories/address-repository.interface.ts
@@ -1,8 +1,7 @@
-import { FindOptionsWhere } from 'typeorm';
 import { AddressModel } from '../models/address.model';
 
 
 export interface IAddressRepository {
   save(data: AddressModel): Promise<AddressModel>;
-  findOne(data: FindOptionsWhere<AddressModel>): Promise<AddressModel | null>;
+  findOne(data: Partial<AddressModel>): Promise<AddressModel | null>;
 }
